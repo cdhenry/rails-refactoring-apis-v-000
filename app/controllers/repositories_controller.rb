a7dc7bb93f88591558143264b83164263b4481fe
Chris Henry <cdhenry@gmail.com>
2018-05-02 13:22:29 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,7 @@
 class RepositoriesController < ApplicationController
   def index
     binding.pry
-    github = GithubService.new(access_hash = {access_token: session[:token]})
+    github = GithubService.new({"access_token" => session[:token]})
     @repos_array = github.get_repos
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     @repos_array = JSON.parse(response.body)
